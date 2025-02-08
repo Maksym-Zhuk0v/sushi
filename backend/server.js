4fce7f804ebbeb6e632a83d80affefb33ccdb7b7
MaksymZhukov <maksymzhukov@AirMaksymZhukov.lan>
2025-02-08 01:19:04 -0500

    Removed embedded git repository in frontend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+import userRoutes from "./routes/user.route.js";
+import blogRoutes from "./routes/blog.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", userRoutes);
+app.use("/api/blog", blogRoutes);
+
+try {
+  app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+    connectDB();
+  });
+} catch (error) {
+  console.log("Error connecting to server", error.message);
+}
